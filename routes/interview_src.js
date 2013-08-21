df97672c8d94f9df3670a63864820c6f8798d3cb
sajus <sajus@cybage.com>
2013-08-21 20:50:59 +0530

    Updated ...

--- routes/interview_src.js
+++ routes/interview_src.js
@@ -3,9 +3,26 @@ var sequelize = require('../dbconfig').sequelize
 
 exports.postInterview = function(req, res){
 	
-	// sequelize.query("INSERT INTO users_tbl (firstname,lastname) VALUES ('"+req.body.firstname+"','"+req.body.lastname+"')").success(function(rows) {
-	// 	console.log("Record inserted successfully");
-	// }).error(function(error) {
-	// 	console.log(error);
-	// });
+	var query = "INSERT INTO interviewresponse_tbl(candiateName, interviewer_1_id, interviewer_2_id, recruiter_id, status_id, round_id, mode_id, description)";
+	query += "VALUES (";
+	query += " "+req.params.candiateName+",";
+	query += " "+req.params.interviewer_1_id+",";
+	query += " "+req.params.interviewer_2_id+",";
+	query += " "+req.params.recruiter_id+",";
+	query += " "+req.params.status_id+",";
+	query += " "+req.params.round_id+",";
+	query += " "+req.params.mode_id+",";
+	query += " "+req.params.description+" ";
+	query += " ); ";
+
+	sequelize.query(query).success(function(rows) {
+		console.log("Record added successfully");
+		res.format({
+			json: function() {
+				res.send(rows);
+			}
+		});
+	}).error(function(error) {
+		console.log(error);
+	});
 };
