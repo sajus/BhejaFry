16187197e9865efb0dfbc9dcb287692af85bbeb3
sajus <sajus@cybage.com>
2013-08-21 19:52:57 +0530

    Added up the handlebar Register Helper.

--- src/js/views/dashboard/dashboardView.js
+++ src/js/views/dashboard/dashboardView.js
@@ -1,5 +1,41 @@
-define(['backbone', 'template!templates/dashboard/dashboard','core','jqueryCookie'],
+define(['backbone', 'template!templates/dashboard/dashboard','core','jqueryCookie','underscore'],
     function(Backbone, dashboardTemplate, Core){
+
+    /* Handlebars register helper to convert Number to readable text */
+    Handlebars.registerHelper('getInterviewer', function(empID) {
+        var emp = _.find(Core.globals.interviewer_list, function(interviewer){
+            return interviewer.empid == empID;
+        })
+        return emp.firstname+" "+emp.lastname;
+    });
+
+    Handlebars.registerHelper('getRecruiter', function(empID) {
+        var emp = _.find(Core.globals.recruiter_list, function(recruiter){
+            return recruiter.empid == empID;
+        })
+        return emp.firstname+" "+emp.lastname;
+    });
+
+    Handlebars.registerHelper('getStatus', function(statusID) {
+        var stat = _.find(Core.globals.interviewstatus_list, function(status){
+            return status.id == statusID;
+        })
+        return stat.status;
+    });
+
+    Handlebars.registerHelper('getRound', function(roundID) {
+        var rnd = _.find(Core.globals.interviewrounds_list, function(rounds){
+            return rounds.id == roundID;
+        })
+        return rnd.round;
+    });
+
+    Handlebars.registerHelper('getMode', function(modeID) {
+        var mod = _.find(Core.globals.interviewmode_list, function(modes){
+            return modes.id == modeID;
+        })
+        return mod.mode;
+    });
 
     var Dashboard = Backbone.View.extend({
 
@@ -9,7 +45,6 @@ define(['backbone', 'template!templates/dashboard/dashboard','core','jqueryCooki
             var self = this;
             this.collection.fetch({
                 success: function() {
-                    console.log(Core);
                     self.$el.html(dashboardTemplate({interviewList: self.collection.toJSON()}));
                 }
             });
