5b3b755f5cf88a2a0442233fdf69f54f765a9fc9
hegdeashwin <intel.cybernetics@gmail.com>
2013-09-07 04:28:39 +0530

    Signed-off-by: hegdeashwin <intel.cybernetics@gmail.com>

--- routes/interview_src.js
+++ routes/interview_src.js
@@ -2,27 +2,19 @@ var sequelize = require('../dbconfig').sequelize
 ,           _ = require('../libresources').underscore;
 
 exports.postInterview = function(req, res){
-
-	console.log(req.body.description);
-
-	var query = "INSERT INTO interviewresponse_tbl(candiateName, interviewer_1_id, interviewer_2_id, recruiter_id, status_id, round_id, mode_id, description)";
+	var query = "INSERT INTO interviewresponse_tbl (candiateName, interviewer_1_id, interviewer_2_id, recruiter_id, status_id, round_id, mode_id, description)";
 	query += "VALUES (";
-	query += " ' " +req.body.candiateName+" ',";
-	query += " ' " +req.body.interviewer_1_id+" ',";
-	query += " ' " +req.body.interviewer_2_id+" ',";
-	query += " ' " +req.body.recruiter_id+" ',";
-	query += " ' " +req.body.status_id+" ',";
-	query += " ' " +req.body.round_id+" ',";
-	query += " ' " +req.body.mode_id+" ',";
-	query += " ' " +req.body.description+" ' )";
+	query += " '" + req.body.candiateName +"',";
+	query += req.body.interviewer_1_id +" ,";
+	query += req.body.interviewer_2_id +" ,";
+	query += req.body.recruiter_id +" ,";
+	query += req.body.status_id +" ,";
+	query += req.body.round_id +" ,";
+	query += req.body.mode_id +" ,";
+	query += " '" +req.body.description +"' )";
 
 	sequelize.query(query).success(function(rows) {
-		console.log("Record added successfully");
-		res.format({
-			json: function() {
-				res.send(rows);
-			}
-		});
+		console.log("Record inserted successfully.")
 	}).error(function(error) {
 		console.log(error);
 	});
--- src/js/models/interview/interviewCreateEditModel.js
+++ src/js/models/interview/interviewCreateEditModel.js
@@ -6,6 +6,15 @@ define(function(require) {
     return Backbone.Model.extend({
         url: function() {
             return Backbone.Model.gateWayUrl + '/interview';
+        },
+
+        validation: {
+            candiateName: {
+                required: true
+            },
+            description: {
+            	required: true
+            }
         }
     });
 });
