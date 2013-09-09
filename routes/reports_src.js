795c64a1f94e59fd53f60cfed75dac3d7ee2c86a
sajus <sajus@cybage.com>
2013-09-09 20:39:29 +0530

    Removed the deleted records from the reports chart service call.

--- routes/reports_src.js
+++ routes/reports_src.js
@@ -10,7 +10,7 @@ exports.getInterviewStatusReport = function(req, res){
 	status_3 = 0
 	status_4 = 0
 	data = {};
-	var query = "SELECT COUNT(status_id) FROM interviewresponse_tbl Where status_id=";
+	var query = "SELECT COUNT(status_id) FROM interviewresponse_tbl Where deleteFlag=0 AND status_id=";
 	sequelize.query(query+"1").success(function(data) {
 		status_1 = data[0]['COUNT(status_id)']
 		sequelize.query(query+"2").success(function(data) {
@@ -45,7 +45,7 @@ exports.getInterviewModeReport = function(req, res){
 	status_2 = 0
 	status_3 = 0 
 	data = {};
-	var query = "SELECT COUNT(mode_id) FROM interviewresponse_tbl Where mode_id=";
+	var query = "SELECT COUNT(mode_id) FROM interviewresponse_tbl Where deleteFlag=0 AND mode_id=";
 	sequelize.query(query+"1").success(function(data) {
 		status_1 = data[0]['COUNT(mode_id)']
 		sequelize.query(query+"2").success(function(data) {
@@ -74,7 +74,7 @@ exports.getInterviewModeReport = function(req, res){
 exports.getInterviewerStatusReport = function(req, res){
 	var query = "SELECT status_id FROM interviewresponse_tbl ";
 	query += "Where (interviewer_1_id="+req.params.id+" OR "
-	query += "interviewer_2_id="+req.params.id+") AND "
+	query += "interviewer_2_id="+req.params.id+") AND deleteFlag=0 AND "
 	query += "status_id=";
 	sequelize.query(query+"1").success(function(rows) {
 		status_1 = rows.length;
@@ -112,8 +112,9 @@ exports.getInterviewerStatusReport = function(req, res){
 exports.getInterviewerModeReport = function(req, res){
 	var query = "SELECT status_id FROM interviewresponse_tbl ";
 	query += "Where (interviewer_1_id="+req.params.id+" OR "
-	query += "interviewer_2_id="+req.params.id+") AND "
+	query += "interviewer_2_id="+req.params.id+") AND deleteFlag=0 AND "
 	query += "mode_id=";
+	console.log(query);
 	sequelize.query(query+"1").success(function(rows) {
 		status_1 = rows.length;
 		sequelize.query(query+"2").success(function(rows) {
